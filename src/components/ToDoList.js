fa645bbe88596600633c00522a1dd36c4b2f435a
Eyob1984 <tamireyob@gmail.com>
2020-06-28 16:04:56 -0700

    file structure created

---
+++ src/components/ToDoList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ToDoItem from './ToDoItems';
+
+function ToDoList(props) {
+    let items = [];
+
+    for (let i = 0; i < props.tasks.length; i++)
+        items.push(
+            <ToDoItem
+                key={i}
+                indx={i}
+                data={props.tasks[i]}
+                modifyTask={props.modifyTask}
+            />,
+        );
+
+    return (
+        <div>
+            <h1>Tasks</h1>
+            {items}
+        </div>
+    );
+}
+
+export default ToDoList;
